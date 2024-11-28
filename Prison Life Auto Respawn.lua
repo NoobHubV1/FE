04deab2a57cf59a4fe142c70fc251f34fc0410bc
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-11-28 13:52:46 +0700

    Create Prison Life Auto Respawn.lua

---
+++ Prison Life Auto Respawn.lua
@@ -0,0 +1,255 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local re2 = Instance.new("TextButton")
+local re1 = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.ResetOnSpawn = false
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Main.BorderColor3 = Color3.fromRGB(255, 0, 0)
+Main.BorderSizePixel = 2
+Main.Position = UDim2.new(0.00485906005, 0, 0.609179854, 0)
+Main.Size = UDim2.new(0.17087847, 0, 0.381620556, 0)
+Main.Draggable = true
+
+re2.Name = "re2"
+re2.Parent = Main
+re2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+re2.Position = UDim2.new(0.0705073774, 0, 0.271171719, 0)
+re2.Size = UDim2.new(0.858350694, 0, 0.453797489, 0)
+re2.Visible = false
+re2.Font = Enum.Font.Arcade
+re2.Text = "Auto Respawn : On"
+re2.TextColor3 = Color3.fromRGB(255, 255, 255)
+re2.TextScaled = true
+re2.TextSize = 14.000
+re2.TextStrokeTransparency = 0.000
+re2.TextWrapped = true
+
+re1.Name = "re1"
+re1.Parent = Main
+re1.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+re1.Position = UDim2.new(0.0705073774, 0, 0.271171719, 0)
+re1.Size = UDim2.new(0.858350694, 0, 0.453797489, 0)
+re1.Font = Enum.Font.Arcade
+re1.Text = "Auto Respawn"
+re1.TextColor3 = Color3.fromRGB(255, 255, 255)
+re1.TextScaled = true
+re1.TextSize = 14.000
+re1.TextStrokeTransparency = 0.000
+re1.TextWrapped = true
+
+local Player, plr = game.Players.LocalPlayer, game.Players.LocalPlayer
+local saved = workspace["Criminals Spawn"].SpawnLocation.CFrame
+
+function GuardsFull(a)
+  if a == "b" then
+    return #game.Teams.Guards:GetPlayers() == 8 and plr.Team == game.Teams.Guards
+  elseif a == "c" then
+    return #game.Teams.Guards:GetPlayers() == 8 and plr.Team ~= game.Teams.Guards
+  end
+end
+function swait()
+  game:GetService("RunService").Stepped:Wait()
+end
+function UnSit()
+  game.Players.LocalPlayer.Character.Humanoid.Sit = false
+end
+function ConvertPosition(Position)
+	if typeof(Position):lower() == "position" then
+		return CFrame.new(Position)
+	else
+		return Position
+	end
+end
+function GetPart(Target)
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+	return Target.Character:FindFirstChild("HumanoidRootPart") or Target.Character:FindFirstChild("Head")
+end
+function GetPosition(Player)
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+	if Player then
+		return GetPart(Player).CFrame
+	elseif not Player then
+		return GetPart(plr).CFrame
+	end
+end
+function GetCameraPosition(Player)
+	return workspace["CurrentCamera"].CFrame
+end
+function Loop(Times, calling)
+	for i = 1, tonumber(Times) do
+		calling()
+	end
+end
+function WaitForRespawn(Cframe,NoForce)
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+	local Cframe = ConvertPosition(Cframe)
+	local CameraCframe = GetCameraPosition()
+	coroutine.wrap(function()
+		local a
+		a = Player.CharacterAdded:Connect(function(NewCharacter)
+			pcall(function()
+				coroutine.wrap(function()
+					workspace.CurrentCamera:GetPropertyChangedSignal("CFrame"):Wait()
+					Loop(2, function()
+						workspace["CurrentCamera"].CFrame = CameraCframe
+					end)
+				end)()
+				NewCharacter:WaitForChild("HumanoidRootPart")
+				local Amount = 8
+				if Player.PlayerGui['Home']['hud']['Topbar']['titleBar'].Title.Text:lower() == "lights out" or Player.PlayerGui.Home.hud.Topbar.titleBar.Title.Text:lower() == "lightsout" then
+					Amount = 11
+				end
+				for i = 1, Amount do
+					UnSit()
+					NewCharacter:WaitForChild("HumanoidRootPart").CFrame = Cframe
+          swait()
+				end
+			end)
+			a:Disconnect()
+			Cframe = nil
+		end)
+		task.spawn(function()
+			wait(2)
+			if a then
+				a:Disconnect()
+			end
+		end)
+	end)()
+end
+
+function ChangeTeam(TeamPath,NoForce,Pos)
+	if TeamPath == game.Teams.Inmates then
+		pcall(function()
+		repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+			game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+			WaitForRespawn(Pos or GetPosition(),NoForce)
+		end)
+		workspace.Remote.TeamEvent:FireServer("Bright orange")
+	elseif TeamPath == game.Teams.Guards then
+		if GuardsFull("b") then
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			workspace.Remote.TeamEvent:FireServer("Bright orange")
+			plr.CharacterAdded:Wait()
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			repeat wait()
+				workspace.Remote.TeamEvent:FireServer("Bright blue")
+			until plr.Team == game.Teams.Guards
+		else
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			workspace.Remote.TeamEvent:FireServer("Bright blue")
+		end
+	elseif TeamPath == game.Teams.Criminals then
+		if Player.Team == game.Teams.Inmates then
+			repeat task.wait()
+				plr.Character.Head.CanCollide = false
+				workspace["Criminals Spawn"].SpawnLocation.CFrame = plr.Character.Head.CFrame
+			until plr.Team == game.Teams.Criminals
+			workspace["Criminals Spawn"].SpawnLocation.CFrame = saved
+		elseif plr.Team == game.Teams.Guards then
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			repeat task.wait()
+				plr.Character.Head.CanCollide = false
+				workspace["Criminals Spawn"].SpawnLocation.CFrame = plr.Character.Head.CFrame
+			until plr.Team == game.Teams.Criminals
+			workspace["Criminals Spawn"].SpawnLocation.CFrame = saved
+		elseif Player.Team == game.Teams.Criminals then
+		if GuardsFull("c") then
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			workspace.Remote.TeamEvent:FireServer("Bright orange")
+			plr.CharacterAdded:Wait()
+			repeat task.wait()
+				plr.Character.Head.CanCollide = false
+				workspace["Criminals Spawn"].SpawnLocation.CFrame = plr.Character.Head.CFrame
+			until plr.Team == game.Teams.Criminals
+			workspace["Criminals Spawn"].SpawnLocation.CFrame = saved
+		else
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			workspace.Remote.TeamEvent:FireServer("Bright blue")
+			plr.CharacterAdded:Wait()
+			pcall(function()
+			repeat task.wait() until game:GetService("Players").LocalPlayer.Character
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+				WaitForRespawn(Pos or GetPosition(),NoForce)
+			end)
+			repeat task.wait()
+				plr.Character.Head.CanCollide = false
+				workspace["Criminals Spawn"].SpawnLocation.CFrame = plr.Character.Head.CFrame
+			until plr.Team == game.Teams.Criminals
+				workspace["Criminals Spawn"].SpawnLocation.CFrame = saved
+		end
+		end
+	elseif TeamPath == game.Teams.Neutral then
+		workspace.Remote.TeamEvent:FireServer("Medium stone grey")
+	end
+end
+function Refresh()
+  ChangeTeam(plr.Team)
+end
+
+re2.MouseButton1Click:connect(function()
+    re2.Visible = false
+    re1.Visible = true
+    AutoRespawn = false
+end)
+
+re1.MouseButton1Click:connect(function()
+    re2.Visible = true
+    re1.Visible = false
+    re1.Text = "Auto Respawn : Off"
+    AutoRespawn = true
+end)
+
+plr.CharacterAdded:Connect(function(NewChar)
+    repeat swait() until NewChar
+    NewChar:WaitForChild('Head')
+    NewChar:WaitForChild("HumanoidRootPart")
+    NewChar:WaitForChild("Humanoid").BreakJointsOnDeath = not AutoRespawn
+    NewChar:WaitForChild("Humanoid").Died:Connect(function()
+        if AutoRespawn then
+          Refresh()
+        end
+    end)
+end)
